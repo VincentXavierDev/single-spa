e207813fb5fae97d1f0735daf30222cd255eff1a
vinhdx <vinhdao992000@gmail.com>
2025-05-05 22:02:57 +0700

    feat: study single spa micro frontend

---
+++ modules/user-management/vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  configureWebpack: {
+    output: {
+      filename: "user-management/user-management.js",
+      libraryTarget: "system",
+    },
+  },
+  transpileDependencies: true,
+});
